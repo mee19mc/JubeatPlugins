1cf638308fbcad5f469e5d6ecd437282e0dc742b
mee19mc <85102106+mee19mc@users.noreply.github.com>
2025-04-15 14:54:25 +0800

    Update profile.ts
    Added references to Avenue

--- jubeat@asphyxia/models/profile.ts
+++ jubeat@asphyxia/models/profile.ts
@@ -253,4 +253,48 @@ export default interface Profile {
 
 
   };
+
+  avenue?: {
+    jubility?: number;
+    jubilityYday?: number;
+    tuneCount?: number;
+    clearCount?: number;
+    saveCount?: number;
+    savedCount?: number;
+    fcCount?: number;
+    exCount?: number;
+    matchCount?: number;
+    bonusPoints?: number;
+    isBonusPlayed?: boolean;
+    totalBestScore?: number;
+    clearMaxLevel?: number;
+    fcMaxLevel?: number;
+    exMaxLevel?: number;
+
+    emblem?: number[];
+    marker?: number;
+    theme?: number;
+    title?: number;
+    parts?: number;
+    sort?: number;
+    category?: number;
+    expertOption?: number;
+    matching?: number;
+    hazard?: number;
+    hard?: number;
+
+    secretList?: number[];
+    themeList?: number;
+    markerList?: number[];
+    titleList?: number[];
+    commuList?: number[];
+    partsList?: number[];
+
+    secretListNew?: number[];
+    themeListNew?: number[];
+    markerListNew?: number[];
+    titleListNew?: number[];
+
+
+  };
 }
